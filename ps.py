8bfb9d0302739ab00e46df200f319727bba92ac6
Hiroyuki_Igusa_mac11 <igusahiroyuki@igusanoMacBook-Air.local>
2024-08-25 14:51:33 +0900

    gitignore updated

--- ps.py
+++
@@ -1,5 +0,0 @@
-from playsound import playsound
-
-# MP3ファイルを再生
-playsound("audio1.wav")
-playsound("sample.mp3")
--- tts.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-
-text = "Is this your pencil?"
-tts = gTTS(text=text, lang='en')
-tts.save("audio1.wav")
